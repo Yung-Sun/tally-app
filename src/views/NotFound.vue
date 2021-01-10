dae632682850402030addc83816f491bd200c08a
SamRong <YungSun3307@163.com>
2021-01-10 22:35:50 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <div>死胡同，看看网址有没有写错</div>
+      <div>
+        <router-link to='/'>返回首页</router-link>
+      </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+export default {
+name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
