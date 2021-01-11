cbbf60181339e47f8e8ebd953363d28f89a166c0
SamRong <YungSun3307@163.com>
2021-01-11 21:22:14 +0800

    引入icons文件夹

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"></use>
+      </svg>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">      <svg>
+      <use xlink:href="#money"></use>
+    </svg>标签</router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">      <svg>
+      <use xlink:href="#statistics"></use>
+    </svg>统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
 export default {
   name: 'Nav'
 };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
-  }
+.nav {
+  border: 1px solid red;
+}
 </style>
