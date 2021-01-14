0eec196d3b7d0885163258fc6e150c7102a8ce41
SamRong <YungSun3307@163.com>
2021-01-14 22:36:02 +0800

    模块化，把Money.vue 分成4个子组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,48 @@
 <template>
-    <div>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </div>
+  <div>
+    <ul class="types">
+      <li :class="type==='-'&&'selected'" @click="selectType('-')">支出</li>
+      <li :class="type==='+'&&'selected'" @click="selectType('+')">收入</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
-name: "Types"
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue {
+  type = '-'; //'-'表示支出,'+'表示收入
+  selectType(type: string) { // type只能为'-'和'+'
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
+    }
+    this.type = type;
+  }
 }
+
+
+// export default {
+//   name: 'Types',
+//   data() {
+//     return {
+//       type: '-' //'-'表示支出,'+'表示收入
+//     }
+//   },
+//   methods: {
+//     selectType(type) {
+//       if (type !== '-' && type !== '+') {
+//         throw new Error('type is unknown')
+//       }
+//       this.type = type
+//       console.log(type === '-')
+//       console.log('selected')
+//       console.log(type === '-' && 'selected')
+//     }
+//   }
+// }
+
 </script>
 
 <style lang="scss" scoped>
