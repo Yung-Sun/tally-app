9c7ac862393512ab9908ec4bc853fbacba44c66c
SamRong <YungSun3307@163.com>
2021-01-15 19:08:46 +0800

    完成Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -9,17 +9,11 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
   type = '-'; // '-'表示支出,'+'表示收入
-
-  @Prop(Number) xxx: number | undefined;
-  // Prop 告诉 Vue xxx 不是 data 是 prop
-  // Number 告诉 Vue xxx 运行时是个 Number
-  // xxx 属性名
-  // number | undefined 告诉 TS xxx 编译时是 number 或者 undefined
 
   selectType(type: string) { // type只能为'-'和'+'中的一种
     if (type !== '-' && type !== '+') {
@@ -28,27 +22,6 @@ export default class Types extends Vue {
     this.type = type;
   }
 }
-
-// export default {
-//   name: 'Types',
-//   data() {
-//     return {
-//       type: '-' //'-'表示支出,'+'表示收入
-//     }
-//   },
-//   methods: {
-//     selectType(type) {
-//       if (type !== '-' && type !== '+') {
-//         throw new Error('type is unknown')
-//       }
-//       this.type = type
-//       console.log(type === '-')
-//       console.log('selected')
-//       console.log(type === '-' && 'selected')
-//     }
-//   }
-// }
-
 </script>
 
 <style lang="scss" scoped>
