178aef082dd3eb26092cded1fb5f08004cae8022
SamRong <YungSun3307@163.com>
2021-01-16 16:05:10 +0800

    监听Notes/NumberPad/Tags/Types组件的值并传到Money.vue中

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,6 @@
 <template>
   <Layout class-prefix="layout">
+    {{record}}
     <NumberPad @update:value="onUpdateAmount"/>
     <Types @update:value="onUpdateType"/>
     <Notes @update:value="onUpdateNote"/>
@@ -13,29 +14,47 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-
 import {Component} from 'vue-property-decorator';
+
+type Record = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+
+}
+
 
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
+  record: Record = {
+    tags:[],
+    notes:'',
+    type:'-',
+    amount: 0
+  }
 
   onUpdateAmount(value: string) {
     console.log(value);
+    this.record.amount = parseFloat(value)
   }
 
   onUpdateType(value: string) {
     console.log(value);
+    this.record.type = value
   }
 
   onUpdateNote(value: string) {
     console.log(value);
+    this.record.notes = value
   }
 
   onUpdateTags(value: string[]) {
     console.log(value);
+    this.record.tags = value
   }
 }
 
