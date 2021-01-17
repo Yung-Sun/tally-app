5afda7f3887960388f40d7a096cceffb99720bc8
SamRong <YungSun3307@163.com>
2021-01-17 23:34:56 +0800

    完成 Not Found 页面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,19 +1,48 @@
 <template>
-    <div>
+  <div class="wrapper">
+    <div class="messageBox">
       <div>死胡同，看看网址有没有写错</div>
       <div>
-        <router-link to='/'>返回首页</router-link>
+        <router-link to='/' class="button">返回首页</router-link>
       </div>
     </div>
+  </div>
 
 </template>
 
 <script lang="ts">
 export default {
-name: "NotFound"
-}
+  name: 'NotFound'
+};
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  position: relative;
+
+  > .messageBox {
+    padding: 10px;
+    border: 1px solid #666666;
+    width: 80vw;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 30%;
+    transform: translate(-50%);
+
+    .button {
+      display: inline-block;
+      color: white;
+      border-radius: 8px;
+      background: #E31F26;
+      padding: 8px;
+      margin-top: 12px;
+    }
+  }
+}
 
 </style>
