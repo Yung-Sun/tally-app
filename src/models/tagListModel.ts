2887940530e5a3f4e00c1c8cc48c922c5b66854d
SamRong <YungSun3307@163.com>
2021-01-20 22:25:36 +0800

    解决 id 生成问题，似乎有bug

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagList';
 type Tag = {
   id: string;
@@ -18,9 +20,11 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name) {
+
     const names = this.data.map(item => item.name);
     if (names.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push({id: name, name: name});
+    const id = createId().toString();
+    this.data.push({ id, name: name});
     this.save();
     return 'success';
   },
