4f72e1b267771724ba8fd9d592f599629b57f8aa
SamRong <YungSun3307@163.com>
2021-01-30 20:49:37 +0800

    保证用户用手机打开页面，否则提示并弹出二维码

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,21 @@ Vue.component('Layout', Layout);
 Vue.component('Icons', Icons);
 
 
-
-
 new Vue({
   router: router,
   store,
   render: h => h(App)
 }).$mount('#app');
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开本页面，以保证浏览效果');
+  const img = document.createElement('img');
+  img.src = '/QR-Code.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top='50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img);
+}
+
